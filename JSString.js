cc7d263ed870e9e457155ae4d6a185176b245359
Crasheldark <ertiojulito@gmail.com>
2025-04-30 10:32:25 +0200

    cadena letra o

--- JSString.js
+++ JSString.js
@@ -9,3 +9,6 @@ console.log(cadena.toUpperCase());
 
 //Muestra por consola la cadena con todos sus caracteres en minúsculas.
 console.log(cadena.toLowerCase());
+
+//Muestra por consola la posición de la cadena en la que se encuentra la letra o.
+console.log(cadena.indexOf("o"));
